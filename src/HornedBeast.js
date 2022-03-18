ce8980acf3d01703d066e4633fe8ce7098190d9c
Tanesha Brester <taneshabrester@gmail.com>
2022-03-18 14:14:58 -0700

    editing images

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,54 +1,57 @@
 import React from "react";
-import Emoji from "./emoji"
-import { Card } from "react-bootstrap";
-import { Button } from "react-bootstrap";
-
+//import Emoji from "./emoji"
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import './HornedBeast.css'
 
 class HornedBeast extends React.Component {
   constructor(props) {
-    super(constructor);
+    super(props);
     this.state = {
-      favorites: "0"
+      favorites: 0,
     }
   }
   //track when a user clicks on an image or one of the beasts
   handleFavorites = () => {
     // const newStatus = this.state.status === "Nay" ? "Yay" : "Nay";
-    this.setState({ favorite: this.state.favorite + 1 })
+    this.setState({ favorite: this.state.favorite + 1 });
     console.log(this.state.favorite);
   };
 
-handleClick = () => {
-  this.fav();
-  this.props.openModal(this.props.beast);
-}
-//referenced bens code from github for this, we have the same names for things "horned beast" and also got help from anthony
+  //for lab 3
+  // handleClick = () => {
+  //   this.handleFavorites();
+  //   // this.props.openModal(this.props.beast);
+  // };
+  //referenced bens code from github for this, we have the same names for things "horned beast" and also got help from anthony
   render() {
     return (
-      <>
+      //<>
+      <article>
         <Card className="h-100">
-          <Card.Img
-            className="imgage"
-            variant="top"
-            alt={this.props.title}
-            src={this.props.image_url}
-            onClick={this.handleClick}
-          />
           <Card.Body>
             <Card.Title>{this.props.title}</Card.Title>
             <Card.Text>♥ {this.state.favorite} ♥</Card.Text>
             <Card.Text>
               {this.props.description}
             </Card.Text>
+            <Card.Img
+              className="imgage"
+              variant="top"
+              alt={this.props.title}
+              src={this.props.image_url}
+              // onClick={this.handleClick}
+            />
             <Button
               variant="primary"
               className="glow-on-hover"
-              onClick={this.fav}>
+              onClick={this.handleFavorites}>
               {/* This Critter is my favorite */}
             </Button>
           </Card.Body>
         </Card>
-      </>
+      </article>
+      //</>
     );
   }
 }
