bc30fa9fcea3d36bd8c88ff767ff8b1c06306e25
Tanesha Brester <taneshabrester@gmail.com>
2022-03-20 14:02:05 -0700

    forms and app editing

---
+++ src/Form.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "react-bootstrap/Form";
+
+class HornedForm extends React.Component {
+  render() {
+    return (
+      <Form>
+        <Form.Label>Horn Quantity</Form.Label>
+        <Form.Group>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select quantity</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="100">One Hundred</option>
+          <option value="All">All</option>
+        </Form.Select>
+        </Form.Group>
+      </Form>
+    )
+  }
+
+}
+
+export default HornedForm
